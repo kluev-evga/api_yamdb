7a53d04dcc46b6d295e0264924eae87dce73665e
Siktorovich <artem-arhipov-99@mail.ru>
2023-03-29 20:53:52 +0300

    Add Titles model

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Titles(models.Model):
+    name = models.CharField(
+        max_length=256,
+        verbose_name='Название',
+    )
+    year = models.PositiveSmallIntegerField(
+        verbose_name='Год выпуска',
+    )
+    description = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name='Описание',
+    )
+    category = models.ForeignKey(
+        Categories,
+        on_delete=models.DO_NOTHING,
+        related_name='category',
+        verbose_name='Категория',
+    )
+
+    def __str__(self):
+        return self.name
