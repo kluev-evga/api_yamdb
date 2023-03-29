ed098b20c247f6cb96d1f3f777a7db65f5efd72d
Siktorovich <artem-arhipov-99@mail.ru>
2023-03-29 22:04:30 +0300

    Add Categories model

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,6 +1,20 @@
 from django.db import models
 
 
+class Categories(models.Model):
+    name = models.CharField(
+        max_length=256,
+        verbose_name='Название',
+    )
+    slug = models.SlugField(
+        unique=True,
+        max_length=50,
+    )
+
+    def __str__(self):
+        return self.name
+
+
 class Titles(models.Model):
     name = models.CharField(
         max_length=256,
