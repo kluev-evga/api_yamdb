7e36711cf31451af3d8c392ba37aebc7737ffb8b
Evgeni <kluev.evga@gmail.com>
2023-04-04 15:29:44 +0300

    genres & categories all PASS

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -3,45 +3,43 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class IsOwnerOrIsAdmin(BasePermission):
     def has_permission(self, request, view):
-        return (
-                request.method in SAFE_METHODS
-                or (request.user.is_authenticated and request.user.is_staff)
-        )  # TODO: Сделать логику superusera
+        return (request.method in SAFE_METHODS or
+                request.user and
+                request.user.is_authenticated and
+                (request.user.is_superuser or
+                 request.user.role == 'admin'))
 
 
 class AdminModeratorOwnerOrReadOnly(BasePermission):
-    def has_object_permission(self, request, view, obj):
-        return (request.method in SAFE_METHODS
-                or request.user.is_admin
-                or request.user.is_staff
-                or obj.author == request.user)
-
     def has_permission(self, request, view):
         return (request.method in SAFE_METHODS
                 or request.user.is_authenticated)
 
+    def has_object_permission(self, request, view, obj):
+        return (request.user and
+                request.user.is_authenticated and
+                (obj.username == request.user.username or
+                 request.user.is_superuser or
+                 request.user.role == 'admin' or
+                 request.user.role == 'moderator'))
+
 
 class IsAdminUser(BasePermission):
     def has_permission(self, request, view):
-        return bool(
-            request.user and
-            request.user.is_authenticated and
-            (request.user.is_superuser or request.user.role == 'admin')
-        )
+        return (request.user and
+                request.user.is_authenticated and
+                (request.user.is_superuser or
+                 request.user.role == 'admin'))
 
 
 class AnyAuthorized(BasePermission):
     def has_permission(self, request, view):
-        return bool(
-            request.user and
-            request.user.is_authenticated)
+        return request.user and request.user.is_authenticated
 
     def has_object_permission(self, request, view, obj):
-        return bool(
-            request.user and
-            request.user.is_authenticated and
-            (obj.username == request.user.username or
-             request.user.is_superuser or
-             request.user.role == 'admin' or
-             request.user.role == 'moderator')
-        )
+        return (request.user and
+                request.user.is_authenticated and
+                (obj.username == request.user.username or
+                 request.user.is_superuser or
+                 request.user.role == 'admin' or
+                 request.user.role == 'moderator'))
