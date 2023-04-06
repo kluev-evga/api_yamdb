b829c71a6bde18758c747116a0c0fe9d9459fd02
Siktorovich <artem-arhipov-99@mail.ru>
2023-04-06 19:38:02 +0300

    Исправил ошибки линтера

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -2,8 +2,8 @@ from datetime import datetime
 
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.validators import UnicodeUsernameValidator
-from django.core.validators import MaxValueValidator, MinValueValidator
 from django.core.exceptions import ValidationError
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from django.utils.translation import gettext_lazy
 
